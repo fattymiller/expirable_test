7ceb73d475290d568a62e2cc826f7c095c0c1d89
David Gil <dgilperez@gmail.com>
2015-03-03 00:15:11 +0100

    locating the currently used arel_table to get it's name or alias

--- app/models/concerns/expirable.rb
+++ app/models/concerns/expirable.rb
@@ -5,27 +5,40 @@ module Expirable
 
       scope :current_and_future, ->(as_at = Time.now) { where("#{upper_bound_column} IS NULL OR #{upper_bound_column} >= ?", as_at) }
       scope :current_and_expired, ->(as_at = Time.now) { where("#{lower_bound_column} IS NULL OR #{lower_bound_column} <= ?", as_at) }
-      
+
       class_attribute :lower_bound_method
       class_attribute :upper_bound_method
-      
+
+      def self.current_arel_table
+        current_table = current_scope.arel.source.left
+
+        case current_table
+        when Arel::Table
+          current_table.name
+        when Arel::Nodes::TableAlias
+          current_table.right
+        else
+          fail
+        end
+      end
+
       def self.lower_bound_field
         lower_bound_method || "effective"
       end
       def self.upper_bound_field
         upper_bound_method || "expire"
       end
-      
+
       def self.lower_bound_column
-        "#{self.table_name}.#{lower_bound_field}"
+        "#{current_arel_table}.#{lower_bound_field}"
       end
       def self.upper_bound_column
-        "#{self.table_name}.#{upper_bound_field}"
+        "#{current_arel_table}.#{upper_bound_field}"
       end
-      
+
       def self.expirable_on(options = {})
         return unless options
-        
+
         self.lower_bound_method = options[:lower].to_s if options[:lower]
         self.upper_bound_method = options[:upper].to_s if options[:upper]
       end
